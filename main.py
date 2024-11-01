e98a050b7a1c8587ec04e5cc0c149441de568a06
Nue666 <ayrton132@yandex.ru>
2024-11-02 00:51:43 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5 * 5)
+
+#2nd program
+print(9.98 < 9.99 and 1000 != 1000.1)
+
+#3rd program
+task1 = 2*2+2
+task2 = 2*(2+2)
+print(task1)
+print(task2)
+print(task1 == task2)
+
+#4th program
+print(int((float('123.456')) * 10) % 10)
